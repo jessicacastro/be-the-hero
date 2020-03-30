3a5cf59610ea548f0882add524c5e60ac62661bd
Jessica Castro <jessica.castros@outlook.com>
2020-03-30 02:42:58 -0300

    Routing - Incident Controller and Ong Controller

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,32 +1,21 @@
 const express = require('express');
-const crypto = require('crypto');
 
-const connection = require('./database/connection');
+const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
 
 const routes = express.Router();
 
-routes.get('/ongs', async (req, res) => {
-    const ongs = await connection('ongs').select('*');
+/**
+ * ONGS ROUTES
+ */
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
 
-    return res.json(ongs);
-});
+/**
+ * INCIDENTS ROUTES
+ */
+routes.post('/incidents', IncidentController.create);
+routes.get('/incidents', IncidentController.index);
 
-routes.post('/ongs', async (req, res) => {
-    const { name, email, whatsapp, city, uf } = req.body;
-
-    const id = crypto.randomBytes(4).toString('HEX');
-
-    await connection('ongs').insert({
-        id,
-        name,
-        email,
-        whatsapp,
-        city,
-        uf
-    });
-
-    return res.json({ id });
-    
-});
 
 module.exports = routes;
